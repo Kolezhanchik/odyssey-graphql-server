1fe002be28b701d0931a17e99b170dcd3f359966
Elena <lenhenlady@gmail.com>
2022-07-11 16:00:42 -0700

    process port

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-const { ApolloServer } = require('apollo-server')
-const typeDefs = require('./schema')
-const resolvers = require('./resolvers')
-const TrackAPI = require('./datasources/track-api')
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const TrackAPI = require('./datasources/track-api');
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI()
-    }
-  }
-})
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI(),
+      };
+    },
+  });
 
+  const { url, port } = await server.listen({port: process.env.PORT||4000});
+  console.log(`
+      🚀  Server is running
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+    `);
+}
 
-server.listen().then(() => {
-  console.log(
-    `🚀  Server is running!
-    🔉  Listening on port 4000
-    📭  Query at https://studio.apollographql.com/dev`
-  )
-})
+startApolloServer(typeDefs, resolvers);
